fc2f61f7b35e95ed59f3261f3a266b8b7c89e2b1
Rawi Daniel <rawidaniel96@gmail.com>
2023-02-02 17:36:45 +0300

    add main

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -22,10 +22,20 @@ Main file
 # formatter = RedactingFormatter(fields=("email", "ssn", "password"))
 # print(formatter.format(log_record))
 
-import logging
+# import logging
 
-get_logger = __import__('filtered_logger').get_logger
-PII_FIELDS = __import__('filtered_logger').PII_FIELDS
+# get_logger = __import__('filtered_logger').get_logger
+# PII_FIELDS = __import__('filtered_logger').PII_FIELDS
 
-print(get_logger.__annotations__.get('return'))
-print("PII_FIELDS: {}".format(len(PII_FIELDS)))
+# print(get_logger.__annotations__.get('return'))
+# print("PII_FIELDS: {}".format(len(PII_FIELDS)))
+
+get_db = __import__('filtered_logger').get_db
+
+db = get_db()
+cursor = db.cursor()
+cursor.execute("SELECT COUNT(*) FROM users;")
+for row in cursor:
+    print(row[0])
+cursor.close()
+db.close()
