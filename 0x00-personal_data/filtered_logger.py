a2e9ca58041bcccdb5a35dc9fcd5ee79b1e410a5
Rawi Daniel <rawidaniel96@gmail.com>
2023-02-02 16:49:31 +0300

    fix bug

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -40,7 +40,7 @@ def get_logger() -> logging.Logger:
     logger.propagate = False
 
     stream_handler = logging.StreamHandler()
-    stream_handler.formatter(RedactingFormatter(list(PII_FIELDS)))
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
     logger.addHandler(stream_handler)
 
     return logger
