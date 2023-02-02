00df9d769b188d33f804604e27cf30dd3cee135f
Rawi Daniel <rawidaniel96@gmail.com>
2023-02-02 16:15:31 +0300

    format log message

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,8 @@ Module filtered_logger
 """
 import re
 from typing import List
+import logging
+import datetime
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -25,3 +27,30 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f'{field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format a string that lists out the attributes
+        that the output should contain
+
+        parameters
+        ----------
+        record: logging.LogRecord
+            LogRecord objects that have all the information related
+            to the event being logged
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
