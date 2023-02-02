8afb2e0189a5487e5c53a56147f6fcace1c076d2
Rawi Daniel <rawidaniel96@gmail.com>
2023-02-02 18:56:12 +0300

    validate hashed password with given password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,7 +12,20 @@ def hash_password(password: str) -> bytes:
     password: str
       password to be hashed
     """
-    # encode = password.encode()
     encode = bytes(password, encoding="utf-8")
     hashed = bcrypt.hashpw(encode, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validate that the provided password matches the hashed password
+
+    Parameters
+    ----------
+    hashed_password: bytes
+        hashed password
+    password: str
+      password to be hashed
+    """
+    encode = bytes(password, encoding="utf-8")
+    return bcrypt.checkpw(encode, hashed_password)
--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -41,8 +41,16 @@ Main file
 # db.close()
 
 
+# hash_password = __import__('encrypt_password').hash_password
+
+# password = "MyAmazingPassw0rd"
+# print(hash_password(password))
+# print(hash_password(password))
+
 hash_password = __import__('encrypt_password').hash_password
+is_valid = __import__('encrypt_password').is_valid
 
 password = "MyAmazingPassw0rd"
-print(hash_password(password))
-print(hash_password(password))
+encrypted_password = hash_password(password)
+print(encrypted_password)
+print(is_valid(encrypted_password, password))
