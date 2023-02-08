97ec796065868d25e6e5e9aeccf1a5d2caf7ceb2
Rawi Daniel <rawidaniel96@gmail.com>
2023-02-08 10:18:17 +0300

    fix bug

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,11 +14,11 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 if getenv("AUTH_TYPE") == 'basic_auth':
-    from api.v1.auth.auth import Auth
-    auth = Auth()
-else:
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+else:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.before_request
