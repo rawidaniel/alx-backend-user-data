ea8a3296c38a895bcb286ef33d1eebf856895040
Rawi Daniel <rawidaniel96@gmail.com>
2023-02-08 10:57:44 +0300

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 Module basic_auth
 """
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -29,3 +31,29 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """Reterive the decoded value of a Base64 string
+           base64_authorization_header
+
+        Parameters
+        ---------
+        base64_authorization_header: str
+            base64 authorization header
+
+        Returns
+        -------
+        str:
+            decoded value of a Base64 string
+        """
+        if base64_authorization_header is None\
+           or type(base64_authorization_header) != str:
+            return
+        try:
+            byte_result = base64.b64decode(base64_authorization_header)
+            str_result = byte_result.decode("utf-8")
+        except binascii.Error as e:
+            return
+        return str_result
