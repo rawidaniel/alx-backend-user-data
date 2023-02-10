d7c0dbcc385ff1c90879753a10eea46d5498ffe0
Rawi Daniel <rawidaniel96@gmail.com>
2023-02-10 15:57:12 +0300

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module session_auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    A class that represent Session Authentication
+    """
+    pass
