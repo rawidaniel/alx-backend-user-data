bc8e27acb9c0056ee064e703303cb83ecd0b82c8
Rawi Daniel <rawidaniel96@gmail.com>
2023-02-13 17:09:05 +0300

    create user

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,3 +1,4 @@
+'''
 #!/usr/bin/env python3
 """
 Main file
@@ -8,3 +9,21 @@ print(User.__tablename__)
 
 for column in User.__table__.columns:
     print("{}: {}".format(column, column.type))
+'''
+
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+from db import DB
+from user import User
+
+my_db = DB()
+
+user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+print(user_1.id)
+
+user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+print(user_2.id)
+
