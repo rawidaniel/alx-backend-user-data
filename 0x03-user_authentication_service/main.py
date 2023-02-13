e8bfeb704c6e4814bb8e4321427b63f7f9142284
Rawi Daniel <rawidaniel96@gmail.com>
2023-02-13 21:06:09 +0300

    Register user

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -93,6 +93,7 @@ except ValueError:
 
 '''
 
+'''
 #!/usr/bin/env python3
 """
 Main file
@@ -100,3 +101,28 @@ Main file
 from auth import _hash_password
 
 print(_hash_password("Hello Holberton"))
+
+'''
+
+#!/usr/bin/env python3
+"""
+Main file
+"""
+from auth import Auth
+
+email = 'me@me.com'
+password = 'mySecuredPwd'
+
+auth = Auth()
+
+try:
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))
+
+try:
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))     
