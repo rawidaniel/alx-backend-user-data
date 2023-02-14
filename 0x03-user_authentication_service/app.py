5f6444b5a53b0f57c428e7d90ba1c5c39bd77193
Rawi Daniel <rawidaniel96@gmail.com>
2023-02-14 12:45:58 +0300

    Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -16,3 +16,20 @@ def home():
     """Home route
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
